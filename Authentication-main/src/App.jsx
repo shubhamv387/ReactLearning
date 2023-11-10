d9ff2797763027dfb2cb2a3e3504dc149d70bbcd
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-11-10 15:48:34 +0530

    saving token to localStorage

--- Authentication-main/src/App.jsx
+++ Authentication-main/src/App.jsx
@@ -1,18 +1,36 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import authContext from './store/auth-context';
 
 function App() {
+  const authCtx = useContext(authContext);
+
   return (
     <Layout>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/profile' component={UserProfile} />
-        <Route exact path='/auth' component={AuthPage} />
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+
+        {!authCtx.isLoggedIn && (
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        )}
+
+        <Route path='/profile'>
+          {authCtx.isLoggedIn ? <UserProfile /> : <Redirect to='/auth' />}
+        </Route>
+
+        <Route path='*'>
+          <Redirect to='/' />
+        </Route>
       </Switch>
     </Layout>
   );
