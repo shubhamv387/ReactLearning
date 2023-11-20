f67ab0279e1a39c04d64c02476d2e347714554a4
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-11-20 16:13:46 +0530

    splitting the redux methods into slices

--- React-Redux/src/store/index.jsx
+++ React-Redux/src/store/index.jsx
@@ -1,47 +1,9 @@
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialCounterState = {
-  counter: 0,
-  showCounter: false,
-};
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    decrement(state, action) {
-      state.counter = state.counter - action.payload;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import AuthReducers from './authSlice';
+import CountReducers from './counterSlice';
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: CountReducers, auth: AuthReducers },
 });
-
-export const counterActions = counterSlice.actions;
-
-export const authActions = authSlice.actions;
 
 export default store;
