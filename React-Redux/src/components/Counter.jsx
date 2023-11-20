9de9ecb62156141a756b5f5c16a711759441e06b
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-11-20 14:31:16 +0530

    added redux toolkit

--- React-Redux/src/components/Counter.jsx
+++ React-Redux/src/components/Counter.jsx
@@ -1,49 +1,44 @@
 import { useSelector, useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
+import { counterActions } from '../store';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button
-          onClick={() => dispatch({ type: 'increment', payload: 1 })}
+          onClick={() => dispatch(counterActions.increment(1))}
           type='button'
         >
           INCREMENT
         </button>
+
         <button
-          onClick={() => dispatch({ type: 'decrement', payload: 1 })}
+          onClick={() => dispatch(counterActions.decrement(1))}
           type='button'
         >
           DECREMENT
         </button>
+
         <button
-          onClick={() => dispatch({ type: 'increment', payload: 2 })}
-          type='button'
-        >
-          INCREMENT BY 2
-        </button>
-        <button
-          onClick={() => dispatch({ type: 'decrement', payload: 2 })}
-          type='button'
-        >
-          DECREMENT BY 2
-        </button>
-        <button
-          onClick={() => dispatch({ type: 'increment', payload: 5 })}
+          onClick={() => dispatch(counterActions.increment(5))}
           type='button'
         >
           INCREMENT BY 5
         </button>
+
         <button
-          onClick={() => dispatch({ type: 'decrement', payload: 5 })}
+          onClick={() => dispatch(counterActions.decrement(5))}
           type='button'
         >
           DECREMENT BY 5
